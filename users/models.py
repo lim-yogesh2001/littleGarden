89a2c51ef91854d65c2929904ca9c7a676a55595
lim-yogesh2001 <yogeshlim2001@gmail.com>
2022-04-19 11:34:41 +0545

    okay

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+
 # Create your models here.
 class User(AbstractUser):
     last_name = None
@@ -10,3 +12,6 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['email']
 
     address = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=100, null=True, default="")
+
+
